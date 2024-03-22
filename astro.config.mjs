e6ff24fa95942ca6051afd1e772e1ba58ee2f4f8
rialsa <ricard.alsina@gmail.com>
2024-03-22 18:10:16 +0100

    📦 NEW: WIP - work on content for first project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
-
+import tailwindcssNesting from 'tailwindcss/nesting'
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()]
+  integrations: [tailwind(), mdx(), react()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [tailwindcssNesting()]
+      }
+    }
+  }
 });
