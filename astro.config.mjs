c489517c836d49e5038106a5b721c4a4fdc15b79
rialsa <ricard.alsina@gmail.com>
2024-03-22 18:46:51 +0100

    🐛 FIX: styles for work page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,11 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
-  vite: {
-    css: {
-      postcss: {
-        plugins: [tailwindcssNesting()]
-      }
-    }
-  }
+  // vite: {
+  //   css: {
+  //     postcss: {
+  //       plugins: [tailwindcssNesting()]
+  //     }
+  //   }
+  // }
 });
